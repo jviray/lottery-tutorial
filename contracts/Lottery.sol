60d23503ed08fb33cfc7302fefb94afd6f75bec7
Jonathan Viray <jfviray7@gmail.com>
2018-10-21 17:55:56 -0400

    Edit Lottery contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -8,19 +8,32 @@ contract Lottery {
         manager = msg.sender;
     }
 
+    modifier onlyManager() {
+        require(msg.sender == manager);
+        _;
+    }
+
     function enter() public payable {
         require(msg.value > .01 ether);
 
         players.push(msg.sender);
     }
 
-    function pickWinner() public {
-        // _random() is being called by this contract, not the address calling pickWinner
+    // Only manager should be able to pick winner; use modifier
+    function pickWinner() public onlyManager {
+        // Note:_random() is being called by this contract, not msg.sender
         uint index = _random() % players.length;
 
-        // Addresses are object with a set of methods
-        // Trasnfers money from current contract to the address calling the method
+        // Note: Addresses are object with a set of methods
+        // Trasnfers ETH from current contract to the address calling the method
         players[index].transfer(this.balance);
+
+        // Re-assigns players with a dynamic address array w/ a length of 0
+        players = new address[](0);
+    }
+
+    function getPlayers() public view returns (address[]) {
+        return players;
     }
 
     function _random() private view returns (uint) {
